46372c4efc1e08fda14c3db7c4e9896e2b56f73c
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-12 11:54:09 +0200

    init

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
-    
+
     public function index(): Response
     {
         return $this->render('article/index.html.twig');
