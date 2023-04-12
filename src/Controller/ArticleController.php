b7f0e5e1fdf98d71e960db77e910094907a05210
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-12 16:55:13 +0200

    Ajout des datas sur la DB et le fetch() fetchAll()

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,79 +2,36 @@
 
 namespace App\Controller;
 
+
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-
 class ArticleController extends AbstractController
 {
+    private ArticleRepository $repoArticle;
+
+    public function __construct(ArticleRepository $repoArticle)
+    {
+        $this->repoArticle = $repoArticle;
+    }
+
     public function show(int $id): Response
     {
-        $articles = $this->getArticles();
-
-
-        if ($id < 0 || $id >= count($articles)) {
-            throw $this->createNotFoundException('Article ya pas.');
-        }
-
-        $article = $articles[$id];
+        $article = $this->repoArticle->find($id);
 
         return $this->render('article/show.html.twig', [
             'article' => $article
         ]);
     }
 
+
     public function index(): Response
     {
-        $articles = $this->getArticles();
+        $articles = $this->repoArticle->findAll();
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles
         ]);
-
-    }
-
-    /**
-     * @return \string[][]
-     */
-    public function getArticles(): array
-    {
-        return [
-            [
-                'titre' => 'Pack de 8 T-shirts Basiques',
-                'description' => 'Obtenez la gamme complète de nos T-shirts basiques. Ayez un nouveau t-shirt toute la semaine et un supplémentaire pour le jour de lessive.',
-                'couleurs' => '8 couleurs',
-                'prix' => '230€',
-                'imageUrl' => 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg'
-            ],
-            [
-                'titre' => 'T-shirt Basique',
-                'description' => 'Ayez l\'air d\'un PDG visionnaire et portez le même t-shirt noir tous les jours.',
-                'couleurs' => 'Noir',
-                'prix' => '29€',
-                'imageUrl' => 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg'
-            ],
-            [
-                'titre' => 'T-shirt à Manches Longues',
-                'description' => 'Restez au chaud et à l\'aise avec notre t-shirt à manches longues de qualité supérieure.',
-                'couleurs' => '5 couleurs',
-                'prix' => '35€',
-                'imageUrl' => 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-03.jpg'
-            ],
-            [
-                'titre' => 'T-shirt Col V',
-                'description' => 'Affichez un style décontracté avec notre t-shirt col V confortable et élégant.',
-                'couleurs' => '6 couleurs',
-                'prix' => '25€',
-                'imageUrl' => 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-04.jpg'
-            ],
-            [
-                'titre' => 'T-shirt Graphique',
-                'description' => 'Exprimez-vous avec notre t-shirt graphique au design unique et accrocheur.',
-                'couleurs' => '4 couleurs',
-                'prix' => '28€',
-                'imageUrl' => 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-05.jpg'
-            ]
-        ];
     }
 }
---
+++ src/Entity/Article.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $couleurs = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageUrl = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCouleurs(): ?string
+    {
+        return $this->couleurs;
+    }
+
+    public function setCouleurs(string $couleurs): self
+    {
+        $this->couleurs = $couleurs;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(string $imageUrl): self
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+}
