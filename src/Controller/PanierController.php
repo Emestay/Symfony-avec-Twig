fe75972a1bfc4d274ec7684aecbcfccb24a7b22b
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-18 09:57:04 +0200

    Ajout des datas sur la DB et le fetch() fetchAll()

---
+++ src/Controller/PanierController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PanierController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('panier/index.html.twig', [
+            'controller_name' => 'PanierController',
+        ]);
+    }
+}
+
